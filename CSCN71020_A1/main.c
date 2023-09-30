e67d12c965bdf0264958bbad505d7614614332f5
Navneet Chatha <navneetchatha@yahoo.ca>
2023-09-29 23:12:40 -0400

    fixed subtraction

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,9 +3,9 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract(); //add subtract
 
-
-void main() {
+int main() {      //changed to int
 
 	printWelcomeMenu();
 
@@ -14,14 +14,21 @@ void main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+	scanf_s("%1d", &inputNum);           //changed to %d for integer
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
+		break;          //added break statement 
+
+	case 2:
+		subtract();         //use the subtract function for option 2
+		break;              //added break statement
+
 	}
 
+	return 0;            //to indicate an execution
 }
 
 void printWelcomeMenu() {
@@ -44,4 +51,14 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;    //calculates and performs subtraction
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
